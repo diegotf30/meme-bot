632ab62297320bf0282f54102351bfc7d386d644
Diego Trevino <diegotf30@gmail.com>
2017-09-16 02:00:55 -0500

    First running build
    The .fit() method from Pillow(PIL fork) doesn't use the template mask
    correctly, the source image ends up stretched, instead of being centered
    properly.

--- Bot.py
+++ Bot.py
@@ -1,1 +1,30 @@
-print('first commit yay!')
+from PIL import Image, ImageOps
+import os, random
+import re
+import linecache
+import string
+
+home_dir = os.path.dirname(os.path.realpath(__file__))
+temp_dir = home_dir + '\\Templates\\'
+source_dir = home_dir + '\\Source\\'
+memes_dir = home_dir + '\\Memes\\'
+
+#Random template & source image
+rand_temp = random.choice(os.listdir(temp_dir))
+rand_source = random.choice(os.listdir(source_dir))
+
+#template
+bot = Image.open(temp_dir + rand_temp)
+
+#source image
+top = Image.open(source_dir + rand_source) 
+
+width, height = bot.size
+size = (width, height)
+
+#Puts Source image in template
+meme = ImageOps.fit(top, size, Image.ANTIALIAS)
+meme.paste(bot, (0,0), bot)
+char_set = string.ascii_uppercase + string.digits
+meme.save( memes_dir + ''.join(random.sample(char_set*6, 6)) + '.png')
+meme.show()
