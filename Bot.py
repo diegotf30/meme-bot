0a9da80ee9e7b0cfa5e4f4427ee1e70f16d1ac1e
Diego Trevino <diegotf30@gmail.com>
2017-09-16 02:13:06 -0500

    Fixed layering problem
    Works with multiple source images, though it doesn't resize the image
    properly nor does it center the image properly on the y-axis.

--- Bot.py
+++ Bot.py
@@ -6,50 +6,64 @@ import string
 
 home_dir = os.path.dirname(os.path.realpath(__file__))
 temp_dir = home_dir + '\\Templates\\'
-source_dir = home_dir + '\\Source\\'
+source_dir = home_dir + '\\Source Images\\'
 memes_dir = home_dir + '\\Memes\\'
 
-#Random template & source image
+
+#Random template
 rand_temp = random.choice(os.listdir(temp_dir))
-rand_source = random.choice(os.listdir(source_dir))
 
 #Get template number
 size_lst = re.findall(r'\d+', rand_temp) #workaround
 size_index = int(''.join(size_lst)) + 2
-#Retrieve box size from file
+#Retrieve blank area from file
 size_s = linecache.getline('sizes.txt',size_index)
-#Blank Area Properties
-size_x = int(size_s.split()[1])
-size_y = int(size_s.split()[2])
 
 #template
-bot = Image.open(temp_dir + rand_temp)
+temp = Image.open(temp_dir + rand_temp)
+#black background
+backg = Image.new('RGB', temp.size)
 
-#source image
-top = Image.open(source_dir + rand_source)
+#Source image counter
+n = 1
 
-#Resizes image to keep aspect ratio
-##Shrink
-if top.size[0] > size_x & top.size[1] > size_y :
-    top.thumbnail((size_x,size_y), Image.LANCZOS)
-##Enlarge
-else:
-    hpercent = (size_y / float(top.size[1])) #Magia pt. 1
-    wsize = int((float(top.size[0]) * float(hpercent))) #Magia 2: The Awakening
-    top = top.resize((wsize, size_y)) #Magia 3: Revenge of the Syntax
+while size_s.split()[n] != 'x' :
+    #Random source image
+    rand_source = random.choice(os.listdir(source_dir))
 
-#Source Image Width
-width = top.size[0]
-#Blank Area - Source Image Width
-black_area = size_x - width 
-#Coordinates where image is pasted
-x = int(size_s.split()[3])
-y = int(size_s.split()[4])
+    #Blank Area Properties
+    size_x = int(size_s.split()[n])
+    size_y = int(size_s.split()[n + 1])
 
-#Puts Source image in template
-bot.paste(top, (int(x + black_area/2),y))
+    #source image
+    src = Image.open(source_dir + rand_source)
+
+    #Resizes image to keep aspect ratio
+    ##Shrink
+    if src.size[0] > size_x & src.size[1] > size_y :
+        src.thumbnail((size_x,size_y), Image.LANCZOS)
+    ##Enlarge
+    else:
+        hpercent = (size_y / float(src.size[1])) #Magia pt. 1
+        wsize = int((float(src.size[0]) * float(hpercent))) #Magia 2: The Awakening
+        src = src.resize((wsize, size_y)) #Magia 3: Revenge of the Syntax
+
+    #Source Image Width
+    width = src.size[0]
+    #Blank Area - Source Image Width
+    black_area = size_x - width 
+    #Coordinates where image is pasted
+    paste_x = int(size_s.split()[n + 2])
+    paste_y = int(size_s.split()[n + 3])
+    #Puts Source image in backgroud
+    backg.paste(src, (int(paste_x + black_area/2),paste_y))
+    #Goes to next source image (if there is a space for it)
+    n = n + 4
+
+#Puts Template on top of background
+backg.paste(temp, (0,0), temp)
+
+#Random filename
 char_set = string.ascii_uppercase + string.digits
-bot.save(memes_dir + ''.join(random.sample(char_set*6, 6)) + '.png')
-
-
-
+backg.save(memes_dir + ''.join(random.sample(char_set*6, 6)) + '.png')
+    
