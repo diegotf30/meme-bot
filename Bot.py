87817a3195db3ed53921bfd934c355deb571804e
Diego Trevino <diegotf30@gmail.com>
2017-09-16 02:19:49 -0500

    Refactored and optimized code

--- Bot.py
+++ Bot.py
@@ -1,7 +1,5 @@
 from PIL import Image
-import os
-import random
-import re
+import os, random
 import linecache
 
 home_dir = os.path.dirname(os.path.realpath(__file__))
@@ -9,13 +7,15 @@ temp_dir = home_dir + '\\Templates\\'
 source_dir = home_dir + '\\Source\\'
 memes_dir = home_dir + '\\Memes\\'
 
+secure_random = random.SystemRandom()
+
 #Random template
-rand_temp = random.choice(os.listdir(temp_dir))
-#Get template number
-size_lst = re.findall(r'\d+', rand_temp) #workaround
-size_index = int(''.join(size_lst)) + 2
-#Retrieve blank area from file
-size_s = linecache.getline('sizes.txt',size_index)
+rand_temp = secure_random.choice(os.listdir(temp_dir))
+#Get template number, eliminates file extension
+temp_num = rand_temp.split('.')[0]
+sizes_index = int(''.join(temp_num)) + 2
+#Retrieve template properties from 'sizes.txt'
+sizes_str = linecache.getline('sizes.txt',sizes_index).split()
 
 #Template
 temp = Image.open(temp_dir + rand_temp)
@@ -24,7 +24,7 @@ temp = Image.open(temp_dir + rand_temp)
 ##b - black
 ##w - white
 ##o - no bg
-bg_color = size_s.split()[1]
+bg_color = sizes_str[1]
 
 if bg_color == 'b' :
     colour = 0
@@ -37,45 +37,47 @@ if bg_color != 'o' :
 else :
     backg = temp
 
-#Name for the file, eliminates '.png' from rand_temp
-meme_name = [rand_temp[:-4]]
+#Name for the file
+meme_name = temp_num.split(' ')
 
-#Source image counter
+#Puts Counter in first box
 n = 2
 
-while size_s.split()[n] != 'x' :
+while n < len(sizes_str) :
     #Determines if the template repeats an image
-    if size_s.split()[n] != 's' :
+    if sizes_str[n] != 's' :
         #Random source image (e.g. '98.png')
-        rand_source = random.choice(os.listdir(source_dir))
-        meme_name.append(rand_source[:-4])
+        rand_source = secure_random.choice(os.listdir(source_dir))
+        #Random source index (e.g. '98')
+        meme_name.append(rand_source.split('.')[0])
     else :
         n += 1
-        
+    
     #Blank Area Properties
-    size_x = int(size_s.split()[n])
-    size_y = int(size_s.split()[n + 1])
+    size_x = int(sizes_str[n])
+    size_y = int(sizes_str[n + 1])
 
     #source image
     src = Image.open(source_dir + rand_source)
 
-    #Resizes image to keep aspect ratio
+    #Resizes image AND keeps aspect ratio
     ##Shrink
-    if src.size[0] > size_x | src.size[1] > size_y :
+    if src.size[0] > size_x or src.size[1] > size_y :
         src.thumbnail((size_x,size_y), Image.LANCZOS)
     ##Enlarge
     else :
-        hpercent = (size_y / float(src.size[1])) #Magia pt. 1
-        wsize = int((float(src.size[0]) * float(hpercent))) #Magia 2: The Awakening
-        src = src.resize((wsize, size_y)) #Magia 3: Revenge of the Syntax
+        hpercent = (size_y / float(src.size[1])) #Magic pt. 1
+        wsize = int((float(src.size[0]) * float(hpercent))) #Magic 2: The Awakening
+        src = src.resize((wsize, size_y)) #Magic 3: Revenge of the Syntax
 
         if src.size[0] > size_x  :
-            wpercent = (size_x / float(src.size[0])) #Magia IV: A New Hope
-            hsize = int((float(src.size[1]) * float(wpercent))) #Magia 5: Wrath of Fam 
-            src = src.resize((size_x, hsize)) #Magia 6: The Return of the Bug
+            wpercent = (size_x / float(src.size[0])) #Magic IV: A New Hope
+            hsize = int((float(src.size[1]) * float(wpercent))) #Magic 5: Wrath of Fam 
+            src = src.resize((size_x, hsize)) #Magic 6: The Return of the Bug
+            
     #Coordinates where image is pasted
-    paste_x = int(size_s.split()[n + 2])
-    paste_y = int(size_s.split()[n + 3])
+    paste_x = int(sizes_str[n + 2])
+    paste_y = int(sizes_str[n + 3])
 
     #Source Image Width
     width = src.size[0]
@@ -83,14 +85,12 @@ while size_s.split()[n] != 'x' :
     #Blank Area - Source Image Width
     black_x = size_x - width
     black_y = size_y - height
-    
-    #Centers Source image in template
+    #Pastes centered Source Image in Template
     backg.paste(src, (int(paste_x + black_x/2),int(paste_y + black_y/2)))
     #Goes to next source image (if there is a space for it)
     n += 4
 
-#Puts Template on top of background
+#Puts Template on top of background (when bg_col = b/w)
 backg.paste(temp, (0,0), temp)
-#Random filename
 #Saves meme
 backg.save(memes_dir + '-'.join(meme_name) + '.png')
